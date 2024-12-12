59cf4e616faa72a25389772f4d711892ac647d27
Yuki.kaco <a0936660023@gmail.com>
2024-12-12 22:09:20 +0800

    Add CustomVariableGUI Open Resize screen.height * 0.85

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,9 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Unity.Mono;
+using HarmonyLib;
 using Naninovel;
+using Naninovel.UI;
 using UnityEngine;
 
 namespace SummerCloverPlugin
@@ -16,10 +18,9 @@ namespace SummerCloverPlugin
         private void Awake()
         {
             Debug.Log("夏色四葉草插件");
-            ScriptMenuKey = Config.Bind<KeyCode>("DebugMenu", "ScriptMenuToggleKey", KeyCode.F1, "測試腳本介面開啟快捷鍵");
-            CustomVariableKey = Config.Bind<KeyCode>("DebugMenu", "CustomVariableToggleKey", KeyCode.F2, "測試自訂變數開啟快捷鍵");
-            DebugGUIKey = Config.Bind<KeyCode>("DebugMenu", "DebugGUIToggleKey", KeyCode.F3, "測試介面開啟快捷鍵");
-
+            CustomVariableKey = Config.Bind<KeyCode>("DebugMenu", "CustomVariableToggleKey", KeyCode.F1, "測試自訂變數開啟快捷鍵/CustomVariableToggleKey");
+            ScriptMenuKey = Config.Bind<KeyCode>("DebugMenu", "ScriptMenuToggleKey", KeyCode.F2, "測試腳本介面開啟快捷鍵/ScriptMenuToggleKey");
+            DebugGUIKey = Config.Bind<KeyCode>("DebugMenu", "DebugGUIToggleKey", KeyCode.F3, "測試介面開啟快捷鍵/DebugGUIToggleKey");
         }
 
         private void Update()
@@ -31,12 +32,27 @@ namespace SummerCloverPlugin
 
             if (Input.GetKeyDown(CustomVariableKey.Value))
             {
+                SetCustomVariableGUIRect();
                 ConsoleCommands.ToggleCustomVariableGUI();
             }
 
             if (Input.GetKeyDown(DebugGUIKey.Value))
             {
                 ConsoleCommands.ToggleDebugInfoGUI();
+            }
+        }
+
+        public void SetCustomVariableGUIRect()
+        {
+            // Access and modify the windowRect field in the CustomVariableGUI class
+            var traverse = Traverse.Create(typeof(CustomVariableGUI)).Field("windowRect");
+            if (traverse.GetValue() is Rect currentRect)
+            {
+                // Modify only the height
+                currentRect.height = (float)Screen.height * 0.85f;
+
+                // Set the updated Rect back to the field
+                traverse.SetValue(currentRect);
             }
         }
     }
